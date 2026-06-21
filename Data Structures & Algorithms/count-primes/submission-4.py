7363f69a67901472a57a214f4bc326a03d081ba4
Xander Morris <xamorris@proton.me>
2026-06-21 12:54:09 -0700

    Add: count-primes - submission-4

---
+++ Data Structures & Algorithms/count-primes/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        sieve = [False] * n
+        res = 0
+
+        for num in range(2, n):
+            if sieve[num]:
+                continue
+
+            res += 1
+
+            if num * num <= n:
+                for i in range(num * num, n, num):
+                    sieve[i] = True
+
+        return res
