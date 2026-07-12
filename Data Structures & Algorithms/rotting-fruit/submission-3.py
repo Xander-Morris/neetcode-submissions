e6583815ebad797af7fff917a78a70ef409c9bf8
Xander Morris <xamorris@proton.me>
2026-07-12 16:28:37 -0700

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        fresh_fruits = minutes_elapsed = 0
+        q = deque()
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 0:
+                    continue
+                elif grid[i][j] == 1:
+                    fresh_fruits += 1
+                elif grid[i][j] == 2:
+                    q.append((i, j))
+
+        # annoying edge case, but if there are no fresh fruits, then it takes no time to rot them (duh)
+        if fresh_fruits == 0:
+            return 0 
+
+        while q:
+            minutes_elapsed += 1 # minutes_elapsed means number of levels in BFS in this problem
+            for _ in range(len(q)):
+                i, j = q.popleft()
+
+                for idelta, jdelta in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+                    ni, nj = i + idelta, j + jdelta
+                    if ni not in range(len(grid)) or nj not in range(len(grid[ni])):
+                        continue
+                    if grid[ni][nj] != 1:
+                        continue
+                    grid[ni][nj] = 2
+                    fresh_fruits -= 1
+                    if fresh_fruits <= 0:
+                        return minutes_elapsed
+                    q.append((ni, nj))
+
+        return -1
