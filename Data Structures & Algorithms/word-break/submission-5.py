20a6a60e3a1fe43fdffd905868bc567abacbbe5b
Xander Morris <xamorris@proton.me>
2026-07-11 19:26:27 -0700

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = {n: True} # want reaching the last index to return True 
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]  
+
+            for word in wordDict:
+                endI = i + len(word)
+                # python's slicing already handles the index being larger than the last index in the string, so no need to manually check that before slicing
+                wordFromS = s[i:endI]
+                if wordFromS == word:
+                    # sliced word matches, so if dfs call on that end index is also True, then dp at this index is True 
+                    if dfs(endI):
+                        dp[i] = True 
+                        return True 
+            
+            dp[i] = False 
+            return False 
+
+        return dfs(0)
