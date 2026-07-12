da0338fd201e451ed901d4da192f8b43c86d7b8b
Xander Morris <xamorris@proton.me>
2026-07-11 20:14:10 -0700

    Add: longest-palindromic-substring - submission-9

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        res = s[0] # resulting substring
+        
+        def helper(i, j):
+            nonlocal res
+            sub = ""
+
+            while i in range(n) and j in range(n):
+                if s[i] != s[j]:
+                    break
+                sub_size = j - i + 1
+                if sub_size > len(res):
+                    # j + 1 since the end index should be included 
+                    # update the result if current substring is larger than current result 
+                    res = s[i:j+1]
+                i -= 1
+                j += 1
+
+        for i, char in enumerate(s):
+            helper(i - 1, i + 1)
+            helper(i, i + 1)
+
+        return res
