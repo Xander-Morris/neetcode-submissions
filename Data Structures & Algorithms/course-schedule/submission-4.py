5db565cd4ca4d7a885578d3fb80f77a2a9417e0b
Xander Morris <xamorris@proton.me>
2026-07-12 17:02:02 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        in_degree = [0] * numCourses
+        adj = [[] for _ in range(numCourses)]
+
+        for a, b in prerequisites:
+            # a is the node that has its indegree incremented because the problem states that
+            # you MUST take course b first if you want to take course a
+            # meaning that a has a new dependency, that being b
+            # indegree = number of dependencies before it = number of edges coming into it
+            in_degree[a] += 1
+            # only go from class before prerequisite to class that depends on it, so no need to add b to a's adjacency list here
+            # only need to add a to b's adjacency list 
+            adj[b].append(a)
+        
+        q = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        visited = 0
+
+        while q:
+            visited += 1
+            node = q.popleft()
+            
+            for nei in adj[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    q.append(nei)
+
+        return visited == numCourses
