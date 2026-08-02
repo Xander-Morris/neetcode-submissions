b0363ffd5442e7b9c0e4d6c92b573c33aaf5b301
Xander Morris <xamorris@proton.me>
2026-08-01 17:28:38 -0700

    Add: number-of-provinces - submission-3

---
+++ Data Structures & Algorithms/number-of-provinces/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        provinces = 0
+        visited = set()
+
+        def _traverse(j):
+            for i, num in enumerate(isConnected[j]):
+                if num != 1:
+                    continue
+                if i in visited:
+                    continue
+                visited.add(i)
+                _traverse(i)
+        
+        for i, inum in enumerate(isConnected):
+            if i in visited:
+                continue
+
+            visited.add(i)
+            provinces += 1
+            _traverse(i)
+
+        return provinces
