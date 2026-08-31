8692d9d0494db71378285fe257806b55d4b86950
Xander Morris <xamorris@proton.me>
2026-08-30 19:30:42 -0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+
+        def _dfs(i, bought):
+            if i == len(prices):
+                return 0
+            if (i, bought) in dp:
+                return dp[(i, bought)]
+            
+            res = _dfs(i + 1, bought)
+
+            if bought:
+                # take max of not selling right now or selling right now
+                res = max(res, prices[i] + _dfs(i + 1, not bought))
+            else:
+                # buy right here
+                res = max(res, -prices[i] + _dfs(i + 1, True))
+            
+            dp[(i, bought)] = res
+
+            return res
+
+        return _dfs(0, False)
